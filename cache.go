9122e8e5c9621b83464af0208047eae940755209
sequix <sequix@163.com>
2021-01-12 22:22:58 +0800

    gc

--- cache.go
+++ cache.go
@@ -2,16 +2,20 @@ package fscache
 
 import (
 	"bytes"
+	"container/heap"
 	"errors"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 	"syscall"
 	"time"
+
+	"github.com/gofrs/flock"
 )
 
-// TODO: lru gc; doc; readme; test; init
+// TODO: doc; readme; test
 
 type Interface interface {
 	Set(key string, src io.Reader) error
@@ -30,16 +34,86 @@ var (
 type FsCache struct {
 	cacheDir   string
 	maxBytes   int64
+	curBytes   int64
 	gcInterval time.Duration
 	fih        fileInfoHeap
+	fihMu      sync.RWMutex
+	gcStopCh   <-chan struct{}
+	gcFlock    *flock.Flock
 }
 
 func New(opts ...Option) (Interface, error) {
-	fc := &FsCache{}
+	fc := &FsCache{
+		cacheDir:   os.TempDir(),
+		gcInterval: 5 * time.Minute,
+	}
 	for _, opt := range opts {
 		opt(fc)
 	}
+	if err := fc.init(); err != nil {
+		return nil, err
+	}
+	if fc.maxBytes > 0 {
+		fc.gcFlock = flock.New(filepath.Join(fc.cacheDir, "gc.lock"))
+		go fc.gcRunner()
+	}
 	return fc, nil
+}
+
+func (fc *FsCache) init() error {
+	return filepath.Walk(filepath.Join(fc.cacheDir, "cache"), func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		fc.curBytes += info.Size()
+		heap.Push(&fc.fih, info)
+		return nil
+	})
+}
+
+func (fc *FsCache) gcRunner() {
+	for {
+		select {
+		case <-fc.gcStopCh:
+			return
+		case <-time.After(fc.gcInterval):
+			fc.gc()
+		}
+	}
+}
+
+func (fc *FsCache) gc() {
+	locked, err := fc.gcFlock.TryLock()
+	if err != nil || !locked {
+		return
+	}
+	defer fc.gcFlock.Unlock()
+
+	fc.fihMu.Lock()
+	defer fc.fihMu.Unlock()
+
+	if fc.curBytes <= fc.maxBytes {
+		return
+	}
+
+	var (
+		needGcBytes = fc.curBytes - fc.maxBytes
+		bytesSoFar  int64
+		keysToGc    []string
+	)
+
+	for bytesSoFar < needGcBytes {
+		fi := heap.Pop(&fc.fih).(os.FileInfo)
+		bytesSoFar += fi.Size()
+		keysToGc = append(keysToGc, fi.Name())
+	}
+
+	for _, k := range keysToGc {
+		if err := os.Remove(fc.filepath(k)); err != nil {
+			return
+		}
+	}
+	fc.curBytes -= bytesSoFar
 }
 
 type Option func(fc *FsCache)
@@ -62,6 +136,12 @@ func WithGcInterval(interval time.Duration) Option {
 	}
 }
 
+func WithGcStopCh(stopCh <-chan struct{}) Option {
+	return func(fc *FsCache) {
+		fc.gcStopCh = stopCh
+	}
+}
+
 func (f *FsCache) filepath(key string) string {
 	return filepath.Join(f.cacheDir, "cache", key)
 }
@@ -70,6 +150,7 @@ func (f *FsCache) tmppath(key string) string {
 	return filepath.Join(f.cacheDir, "tmp", key)
 }
 
+// TODO update curBytes
 func (f *FsCache) Set(key string, src io.Reader) error {
 	return atomicWriteFile(f.filepath(key), f.tmppath(key), src, 0644)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sequix/fscache
 
 go 1.15
 
-require golang.org/x/sys v0.0.0-20210110051926-789bb1bd4061 // indirect
+require (
+	github.com/gofrs/flock v0.8.0 // indirect
+	golang.org/x/sys v0.0.0-20210110051926-789bb1bd4061 // indirect
+)
